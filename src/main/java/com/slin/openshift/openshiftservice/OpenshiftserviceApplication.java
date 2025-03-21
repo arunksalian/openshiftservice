acab5778796cb5e3b8d8952d520c1e03f392cb8b
Arun.k <arun.k@cavininfotec.com>
2025-03-21 18:39:09 +0530

    src added

---
+++ src/main/java/com/slin/openshift/openshiftservice/OpenshiftserviceApplication.java
@@ -0,0 +1,13 @@
+package com.slin.openshift.openshiftservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenshiftserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenshiftserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slin/openshift/openshiftservice/OpenshiftserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slin.openshift.openshiftservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenshiftserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
